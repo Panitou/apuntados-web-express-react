9ec9b21fcfc3b651a6ae5dd9c8a2238d095c449a
Panitou <joaco0mr4@gmail.com>
2024-06-10 22:10:07 -0500

    create header component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
-    return (
-        <div className='text-black'>Home</div>
-    )
+  return <div className="text-black">Home</div>;
 }
 
-export default Home
+export default Home;
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Signin() {
-    return (
-        <div>SignIn</div>
-    )
+  return <div>SignIn</div>;
 }
 
-export default Signin
+export default Signin;
