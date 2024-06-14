bc7f40c64807fd5c11a54ea3d0ec9563b7cf029f
Panitou <joaco0mr4@gmail.com>
2024-06-14 03:03:37 -0500

    create signin api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -70,7 +70,7 @@ function SignUp() {
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? "Loading" : "Sign Up"}
-        </button>
+        </button
       </form>
       <div className="flex gap-2 mt-5">
         <p>¿Tienes cuenta?</p>
