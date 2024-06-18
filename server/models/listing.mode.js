7c02f7c4a47c51eb9cb714245856af6baac907a0
Panitou <joaco0mr4@gmail.com>
2024-06-17 19:57:11 -0500

    add create listing api route

---
+++ server/models/listing.mode.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    course: {
+      type: String,
+      required: true,
+    },
+    semester: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
