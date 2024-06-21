31e5df3ad7d16275d44efbe73940e80cd8a40e58
Panitou <joaco0mr4@gmail.com>
2024-06-21 14:38:43 -0500

    complete show user functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -54,7 +54,7 @@ export const getUserListings = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
       const listing = await Listing.find({ userRef: req.params.id });
-      res.status(200).json(listings);
+      res.status(200).json(listing);
     } catch (error) {
       next(error);
     }
