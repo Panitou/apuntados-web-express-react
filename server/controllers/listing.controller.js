72118552cfecd9edbad0f4e6f3fe841f50da8e64
Panitou <joaco0mr4@gmail.com>
2024-06-21 16:29:01 -0500

    create search api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -58,3 +58,23 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit);
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      semester,
+      course,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+    return res.status(200).json(listings);
+  } catch (error) {}
+};
