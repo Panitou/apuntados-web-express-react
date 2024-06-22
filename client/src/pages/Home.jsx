7f5f2359f6732c42084e43f50623141d8d696590
Panitou <joaco0mr4@gmail.com>
2024-06-22 01:21:49 -0500

    add pre installation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div className="text-black">Home</div>;
+  return (
+    <div>
+      {/* top */}
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Consigue los mejores{" "}
+          <span className="text-slate-500">apuntes universitarios</span>
+          <br />
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Compra apuntes de diferentes personas y universidades, y vende y gana
+          una comisión por cada venta.
+        </div>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+        >
+          Ver apuntes
+        </Link>
+      </div>
+      {/* swiper */}
+      {/* listing results */}
+    </div>
+  );
 }
 
 export default Home;
